e673b6d1e276ec75c58819f09d44b7d3cd9b4ed8
francissdo <francissdo@gmail.com>
2025-08-24 00:53:49 -0400

    ...

--- DtlApi/Data/Interfaces/ICategoryRepository.cs
+++
@@ -1,9 +0,0 @@
-using DtlApi.Models;
-
-namespace DtlApi.Data.Interfaces;
-
-public interface ICategoryRepository : IRepository<Category>
-{
-    Task<IEnumerable<Category>> GetActiveCategoriesAsync();
-    Task<Category?> GetCategoryWithProductsAsync(int id);
-}
